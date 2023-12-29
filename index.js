d13e3cdc2f648e0703b6cda768e65fdfdb708e2d
Alimova Lilya <lily@morion.tech>
2023-12-29 17:00:28 +0300

    v1.0

--- index.js
+++ index.js
@@ -8,7 +8,25 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true,
 }));
-app.get('/', (request, response) => {
+
+
+app.get('/', async (request, response) => {
+    
+    const url = request.url.split("?")[1].split("=")[1]
+    console.log("request: ", url)
+
+    try {
+        const apiResponse = await fetch(
+         'https://smsc.ru/sys/send.php?login=Chopho&psw=V-Power423&phones=79172473509&mes=hello')
+         console.log("Send Result: ", apiResponse)
+
+        // const apiResponseJson = await apiResponse.json()
+        // console.log("Send OK: ", apiResponseJson)
+        
+    } catch (err) {
+        console.log("Send Error: ", err)
+    }
+
     response.send({
         message: 'Node.js and Express REST API'}
     );
